1721c976fbb462d2ae56dd4ed754af9be7cce85c
milaronix <milaronix@gmail.com>
2015-02-10 22:26:37 -0600

    primer commit

---
+++ app/src/main/java/com/tareassp1/milaronix/tareassp1/MainActivity.java
@@ -0,0 +1,71 @@
+package com.tareassp1.milaronix.tareassp1;
+
+/**
+ * Created by Milaronix on 25/01/2015.
+ */
+
+import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final String[] nombres = {"Pollo Campero", "Taco Bell", "Pizza Hut", "Go Green", "Subway", "Burguer King"};
+        final String[] direcciones = {"20 calle 4-65 zona 1", "20 calle 4-65 zona 2", "20 calle 4-65 zona 3", "20 calle 4-65 zona 4", "20 calle 4-65 zona 5", "21256789"};
+        final String[] telefonos = {"24203536", "23232525", "21256363", "21256365", "21259854", "21256789"};
+        ListView lv = (ListView) findViewById(R.id.list);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent i = new Intent(getApplicationContext(),midetalle.class);
+                i.putExtra("elnombre", nombres[position]);
+                i.putExtra("ladireccion", direcciones[position]+", Guatemala, Guatemala");
+                i.putExtra("eltelefono", telefonos[position]);
+                i.putExtra("elmail",telefonos[position]+"@gmail.com");
+                i.putExtra("elwebsite","www."+telefonos[position]+".com");
+                startActivity(i);
+            }
+        });
+
+        lv.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, nombres));
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/tareassp1/milaronix/tareassp1/midetalle.java
@@ -0,0 +1,69 @@
+package com.tareassp1.milaronix.tareassp1;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.text.util.Linkify;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+public class midetalle extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_midetalle);
+
+        TextView textoEst = (TextView) findViewById(R.id.nombreEst);
+        TextView direccionEst = (TextView) findViewById(R.id.dirEst);
+        TextView telefonoEst = (TextView) findViewById(R.id.telEst);
+        TextView correoEst = (TextView) findViewById(R.id.mailEst);
+        TextView websiteEst = (TextView) findViewById(R.id.sitioEst);
+
+        Intent i = getIntent();
+
+        String nombre = i.getStringExtra("elnombre");
+        textoEst.setText(nombre);
+
+        String direccion = i.getStringExtra("ladireccion");
+        direccionEst.setText(direccion);
+        Linkify.addLinks(direccionEst,Linkify.MAP_ADDRESSES);
+
+        String telefono = i.getStringExtra("eltelefono");
+        telefonoEst.setText(telefono);
+        Linkify.addLinks(telefonoEst,Linkify.ALL);
+
+        String mail = i.getStringExtra("elmail");
+        correoEst.setText(mail);
+        Linkify.addLinks(correoEst,Linkify.ALL);
+
+        String sitio = i.getStringExtra("elwebsite");
+        websiteEst.setText(sitio);
+        Linkify.addLinks(websiteEst,Linkify.ALL);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_midetalle, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
