4897362f8ba8fa32bdcfbf1c296f9cc4cd260e82
milaronix <milaronix@gmail.com>
2015-02-14 22:05:11 -0600

    ya tiene tabs vacias

--- app/src/main/java/com/tareassp1/milaronix/tareassp1/MainActivity.java
+++ app/src/main/java/com/tareassp1/milaronix/tareassp1/MainActivity.java
@@ -4,11 +4,12 @@ package com.tareassp1.milaronix.tareassp1;
  * Created by Milaronix on 25/01/2015.
  */
 
-import android.app.ActionBar;
+//import android.app.ActionBar;
 import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -26,6 +27,34 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        ActionBar.TabListener tabListener = new ActionBar.TabListener() {
+            @Override
+            public void onTabSelected(ActionBar.Tab tab, android.support.v4.app.FragmentTransaction fragmentTransaction) {
+                Toast.makeText(getApplicationContext(),tab.getText(),Toast.LENGTH_SHORT);
+
+            }
+
+            @Override
+            public void onTabUnselected(ActionBar.Tab tab, android.support.v4.app.FragmentTransaction fragmentTransaction) {
+
+            }
+
+            @Override
+            public void onTabReselected(ActionBar.Tab tab, android.support.v4.app.FragmentTransaction fragmentTransaction) {
+
+            }
+        };
+
+        for(int i = 1; i < 5; i++){
+            actionBar.addTab(
+                    actionBar.newTab()
+                    .setText("Tab " + i)
+                    .setTabListener(tabListener));
+        }
     }
 
 
